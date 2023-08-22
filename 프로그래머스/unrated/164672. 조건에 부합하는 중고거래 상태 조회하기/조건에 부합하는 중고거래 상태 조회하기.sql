256fca14cf5b8f1b6f9dfc78052cc20a97071851
Raphael <116366409+HyoJoon-Yang@users.noreply.github.com>
2023-08-22 16:26:30 +0900

    [unrated] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -9,20 +9,11 @@
 -- WHERE CREATED_DATE LIKE '2022-10-05'
 -- ORDER BY BOARD_ID DESC;
 
-
-SELECT
-
-        BOARD_ID
-       ,WRITER_ID
-       ,TITLE
-       ,PRICE
-       ,CASE WHEN STATUS = 'SALE' THEN '판매중'
-             WHEN STATUS = 'RESERVED' THEN '예약중'
-             WHEN STATUS = 'DONE' THEN '거래완료' 
-             ELSE 'Unknown'
-         END AS STATUS
-
-FROM USED_GOODS_BOARD
-WHERE 1=1
-AND TO_CHAR(CREATED_DATE,'YYYY-MM-DD') = '2022-10-05'
-ORDER BY BOARD_ID DESC
+SELECT board_id, writer_id, title, price,
+CASE WHEN status = 'SALE' THEN '판매중'
+     WHEN status = 'RESERVED' THEN '예약중'
+     WHEN status = 'DONE' then '거래완료'
+END AS status
+from used_goods_board
+where to_char(created_date,'yyyy-mm-dd') = '2022-10-05'
+order by 1 desc;
